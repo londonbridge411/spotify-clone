470894b4f36ae54abc0fbf4e56c0bf5da133c01c
London Bowen <londonbridge411@gmail.com>
2024-03-20 16:50:19 -0400

    Removed print stms

--- src/components/Containers/ContextMenus/Playlist Context Features/RenamePlaylist.tsx
+++ src/components/Containers/ContextMenus/Playlist Context Features/RenamePlaylist.tsx
@@ -12,15 +12,12 @@ export var RenamePlaylist_Exported: any;
 export default function ContextOption_RenamePlaylist(props: any) {
   const [id, setID] = useState(""); // Used in onclick to update the props.target. For some reason, I need this
 
-  console.log(props.target);
   function RenamePlaylist() {
     let run = async () => {
       var new_name: string = (
         document.getElementById("rename-playlist-name") as HTMLInputElement
       )?.value;
 
-      console.log(props.target);
-      console.log(new_name);
       await supabase
         .from("Playlists")
         .update({ name: new_name })
