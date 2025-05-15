f062a3b178c3e6734fb5d16b1a4010eee4eea41d
londonbridge411 <londonbridge411@gmail.com>
2025-05-15 19:54:05 -0400

    Update DeletePlaylist.tsx

--- src/components/Containers/ContextMenus/Playlist Context Features/DeletePlaylist.tsx
+++ src/components/Containers/ContextMenus/Playlist Context Features/DeletePlaylist.tsx
@@ -39,36 +39,20 @@ export default function ContextOption_DeletePlaylist(props: any) {
               .remove(["pictures/backgrounds/" + props.target]);
           }
 
-          // Delete followers
+          // Delete Each Song
           await supabase
-            .from("Users")
-            .select("id, subscribed_playlists")
-            .contains("subscribed_playlists", [props.target])
+            .from("Songs_Playlists")
+            .select("song_id")
+            .eq("playlist_id", props.target)
             .then(async (result) => {
-              const subs = result.data;
-
-              if (subs != null) {
-                //console.log(subs);
-                for (let i = 0; i < subs?.length; i++) {
-                  //console.log(subs.at(i)?.subscribed_playlists);
-
-                  const subbed_playlists: string[] =
-                    subs.at(i)?.subscribed_playlists;
-
-                  subbed_playlists.splice(
-                    subbed_playlists.indexOf(props.target),
-                    1
-                  );
-
-                  //console.log(subbed_playlists);
-                  console.log("Passed"); //Good
-                  await supabase
-                    .from("Users")
-                    .update({ subscribed_playlists: subbed_playlists })
-                    .eq("id", subs.at(i)?.id);
-                }
+              if (result?.data != null)
+              {
+                for (let i = 0; i < result.data.length; i++)
+                  await DeleteSong(result.data[i].song_id);
               }
-            });
+
+            })
+          //
 
           // Delete Row
           console.log(props.target);
@@ -89,32 +73,7 @@ export default function ContextOption_DeletePlaylist(props: any) {
   async function DeleteSong(song_id: string) {
     // Remove from each playlist that has it.
 
-    await supabase
-      .from("Playlists")
-      .select("id, song_ids")
-      .contains("song_ids", [song_id])
-      .then(async (result) => {
-        const playlists_with_song: object[] = result.data!;
-
-        for (let i = 0; i < playlists_with_song.length; i++) {
-          // Printing out the first list
-          const songs: any = playlists_with_song[i];
-          songs.song_ids.splice(songs.song_ids.indexOf(song_id), 1); // Remove the song from the index
-
-          await supabase
-            .from("Playlists")
-            .update({ song_ids: songs.song_ids })
-            .eq("id", songs.id);
-        }
-
-        // Remove Song from Songs Table
-        await supabase.from("Songs").delete().eq("id", song_id);
-
-        // Remove file associated with it.
-        supabase.storage.from("music-files").remove(["audio-files/" + song_id]);
-
-        //window.location.reload();
-      });
+    await supabase.storage.from("music-files").remove(["audio-files/" + song_id]);
   }
 
   return (
